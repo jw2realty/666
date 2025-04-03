d1f0662865af1f9be1a54099b5517837e35265b7
jw2realty <jw2realtyteam@gmail.com>
2025-04-03 13:27:51 -0400

    666

---
+++ app-files/data.js
@@ -0,0 +1,156 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-entryway",
+      "name": "1. Entryway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.09193604970165836,
+        "pitch": 0.0066740045076834065,
+        "fov": 1.232273651715491
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.039867439615377975,
+          "pitch": 0.21494780650377265,
+          "rotation": 0.7853981633974483,
+          "target": "1-3-living-roomdining"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-3-living-roomdining",
+      "name": "3. Living Room/Dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.0064197867379931495,
+        "pitch": 0.009060835674397083,
+        "fov": 1.232273651715491
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.28803999061141106,
+          "pitch": 0.42343639724543713,
+          "rotation": 6.283185307179586,
+          "target": "2-5-kitchendining"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-5-kitchendining",
+      "name": "5. Kitchen/Dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.21510263142624098,
+        "pitch": 0.027065892722053064,
+        "fov": 1.232273651715491
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.768190060420606,
+          "pitch": 0.5551651629820658,
+          "rotation": 1.5707963267948966,
+          "target": "3-7-kitchenstairs"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-7-kitchenstairs",
+      "name": "7. Kitchen/Stairs",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -2.751281472071362,
+        "pitch": 0.4440690416431643,
+        "fov": 1.232273651715491
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Brandywine",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
